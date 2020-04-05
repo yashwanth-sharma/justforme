d18f7538ac6ef584bc742f70d0b90a07d2ef3972
Yashwanth Sharma <55616104+yashwanth-sharma@users.noreply.github.com>
2020-04-05 17:50:28 +0530

    just updated the input statement

--- identitymatrix.py
+++ identitymatrix.py
@@ -1,5 +1,5 @@
 # identitymatrix
-n=int(input("enter the number:"))
+n=int(input("enter the number of rows needed for the identity matrix:"))
 for i in range(0,n):
     for j in range(0,n):
         if(i==j):
